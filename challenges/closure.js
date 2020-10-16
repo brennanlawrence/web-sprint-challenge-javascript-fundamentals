f1c9301cbb9847e09a589cfff913205dcdddbf9d
brennanlawrence <brennanlawrence2002@gmail.com>
2020-10-16 10:24:58 -0600

    Finished Closure

--- challenges/closure.js
+++ challenges/closure.js
@@ -18,7 +18,21 @@ myFunction();
 
 // Explanation: 
 
+/*The internal variable may be inside the function myFunction, but it is still outside of the nestedFunction. When nestedFunction is invoked it first looks inside itself for the variable internal, when it is unsuccessful it turns outword and finds the definition of the variable. Thus, since both internal variable and nested function live on the same scope, nestedFunction doesn't have any scope issues.
+
+
+*/
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function summation(num){
+  let newNum = 0;
+  for(let i = 0; i <= num; i++) {
+    newNum += i;
+  }
+  return newNum;
+}
+
+console.log(summation(4));
